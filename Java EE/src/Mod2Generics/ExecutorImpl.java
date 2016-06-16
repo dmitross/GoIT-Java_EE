2ac72b1b057af8cc1cd07907a16d9b9209025d39
dmitross <dmitros632@gmail.com>
2016-06-16 17:56:23 +0300

    addition

--- Java EE/src/Mod2Generics/ExecutorImpl.java
+++ Java EE/src/Mod2Generics/ExecutorImpl.java
@@ -3,23 +3,75 @@ package Mod2Generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExecutorImpl<T> implements Task<T>{
+public class ExecutorImpl<T> implements Executor<T>{
 
-    List<T> tasks = new ArrayList<>();
-    List<Task> results = new ArrayList<>();
+    private static final Validator<Object> DEFAULT_VALIDATOR = value -> true;
+    private List<TaskAndValidator<T>> tasks = new ArrayList<>();
+    private List<T> validResults = new ArrayList<>();
+    private List<T> invalidResults = new ArrayList<>();
+    private boolean executed = false;
 
-    public void addTask (T task) {
-        tasks.add(task);
+    @Override
+    public void addTask(Task<? extends T> task) {
+        checkNotExecuted();
+        addTask(task, DEFAULT_VALIDATOR);
     }
 
+    @Override
+    public void addTask(Task<? extends T> task, Validator<? super T> validator) {
+        tasks.add(new TaskAndValidator<T>(task, validator));
+
+    }
+
+    @Override
     public void execute() {
-        for (T t : tasks) {
-            t.execute();
+        checkNotExecuted();
+        for (TaskAndValidator<T> taskAndValidator : tasks) {
+            Task<? extends T> task = taskAndValidator.tasks;
+            task.execute();
+            if (taskAndValidator.validator.isValid(task.getResult())) {
+                validResults.add(task.getResult());
+            } else {
+                invalidResults.add(task.getResult());
+            }
+        }
+
+        executed = true;
+    }
+
+    @Override
+    public List<T> getValidResults() {
+        checkExecuted();
+        return validResults;
+    }
+
+
+    @Override
+    public List<T> getInvalidResults() {
+        checkExecuted();
+        return invalidResults;
+    }
+
+    private void checkNotExecuted() {
+        if (executed) {
+            throw new IllegalStateException("Executor already executed");
         }
     }
 
-    public T getResult() {
-        return (T) results;
+    private void checkExecuted() {
+        if (!executed) {
+            throw new IllegalStateException("Executor already executed");
+        }
+    }
+
+    private class TaskAndValidator<T> {
+        private Task<? extends T> tasks;
+        private Validator<? super T> validator;
+
+        public TaskAndValidator(Task<? extends T> tasks, Validator<? super T> validator) {
+            this.tasks = tasks;
+            this.validator = validator;
+        }
     }
 }
 
